0d9891b08687ebe78a8f58becaa6cf085d10c2d9
Samriddhi Chaturvedi <135617062+samriddhi20s@users.noreply.github.com>
2025-02-20 00:55:43 +0530

    Create Ex-5-11.py

---
+++ More Classes/Ex-5-11.py
@@ -0,0 +1,22 @@
+#Static Variables and Methods: Explore the use of static variables and methods in a class.
+#Example: Track the number of instances created for a class using a static variable.
+class MyClass:
+    # Static variable to count instances
+    instance_count = 0
+
+    def __init__(self):
+        # Increment the instance count every time a new instance is created
+        MyClass.instance_count += 1
+
+    @staticmethod
+    def get_instance_count():
+        # Static method to access the static variable
+        return MyClass.instance_count
+
+# Creating instances of the class
+obj1 = MyClass()
+obj2 = MyClass()
+obj3 = MyClass()
+
+# Accessing the static method to get the count of instances
+print(f"Number of instances created: {MyClass.get_instance_count()}")  # Should print 3
