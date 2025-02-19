e35e785648c4a91ec7cfc306d90761568215bc6b
Samriddhi Chaturvedi <135617062+samriddhi20s@users.noreply.github.com>
2025-02-20 00:37:00 +0530

    Create Ex-4-18.py

---
+++ Decorators and lambda functions/Ex-4-18.py
@@ -0,0 +1,10 @@
+#Advanced List Comprehension: Implement more complex logic in a list comprehension.
+#Create a list comprehension that transforms all strings in a list to uppercase and all integers to their square values.
+
+input_list = ["hello", 3, "world", 5, "python", 2]
+
+# List comprehension with conditional logic
+output_list = [item.upper() if isinstance(item, str) else item**2 if isinstance(item, int) else item for item in input_list]
+
+# Output the transformed list
+print(output_list)
