01e9504d2b27575661b991aa2fdbcaec783ef885
Samriddhi Chaturvedi <135617062+samriddhi20s@users.noreply.github.com>
2025-02-20 00:24:49 +0530

    Create Ex-4-11.py

---
+++ Decorators and lambda functions/Ex-4-11.py
@@ -0,0 +1,4 @@
+#Simple List Comprehension: Practice creating a list comprehension.
+#Generate a list of squares for numbers from 1 to 10 using a list comprehension.
+squares = [x**2 for x in range(1, 11)]
+print(squares)
