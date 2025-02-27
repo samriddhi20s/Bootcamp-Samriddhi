45fa788b18c399bed0877ee541904dd01deca89f
Samriddhi Chaturvedi <135617062+samriddhi20s@users.noreply.github.com>
2025-02-27 09:52:05 +0530

    Delete Persistence_queue_system/data directory

--- Persistence_queue_system/data/ops_console.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-from persistent_queue.sqlite_queue import SQLiteQueue
-
-db = SQLiteQueue('queue.db')
-
-st.title('Ops Console')
-jobs = db.get_all_jobs()
-st.write('Pending Jobs:', jobs)
